a55d2f15ff1951f0bf8705812fc4afba2e112d39
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2020-10-25 21:10:34 -0300

    add Chromosome class
    - This class represents an individual in the Genetic Algorithm;

---
+++ src/Entities/Chromosome.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace EvilComp\Entities;
+
+use EvilComp\Handlers\TaskListHandler;
+use EvilComp\Handlers\TaskHandler;
+
+/**
+ * Class Chromosome
+ * @author yourname
+ */
+class Chromosome
+{
+    protected $tasks;
+    protected $processors;
+    protected $size;
+
+    public function __construct(array $tasks = [], array $processors = [])
+    {
+        $this->size = count($tasks);
+        $this->tasks = $tasks;
+        $this->processors = $processors;
+    }
+
+    public function dump() {
+        var_dump(
+            implode(' ', $this->tasks),
+            implode(' ', $this->processors)
+        );
+    }
+
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+
+    public function hasTask($taskId)
+    {
+        return in_array($taskId, $this->tasks);
+    }
+
+    public function getProcessors()
+    {
+        return $this->processors;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function addTask($index, $taskId, $coreId)
+    {
+        if (isset($this->tasks[$index])) {
+            $this->tasks[$index] = $taskId;
+            $this->processors[$index] = $coreId;
+            return $this;
+        }
+
+        $this->tasks[$index] = $taskId;
+        $this->processors[$index] = $coreId;
+        ++$this->size;
+        return $this;
+    }
+
+    public function correct(TaskListHandler $taskListHandler)
+    {
+        $i = 0;
+        while ($i < $this->getSize()) {
+            $gene = $this->tasks[$i];
+
+            $taskDependency = [];
+            foreach ($taskListHandler->getTaskLists() as $l) {
+                $dependencies = $l->getDependencies($gene);
+
+                if (!$dependencies) {
+                    continue;
+                }
+
+                foreach ($dependencies as $dependency) {
+                    $taskPos = array_search($dependency, $this->tasks);
+
+                    if ($taskPos < $i) {
+                        continue;
+                    }
+
+                    $taskDependency[$taskPos] = $dependency;
+                }
+            }
+
+            if (!$taskDependency) {
+                ++$i;
+                continue;
+            }
+
+            asort($taskDependency);
+
+            $taskPos = array_search(array_shift($taskDependency), $this->tasks);
+
+            $this->swap($i, $taskPos);
+
+            $i = min($i, $taskPos);
+        }
+
+        return $this;
+    }
+
+    public function swap($posA, $posB)
+    {
+        $tmp = $this->tasks[$posB];
+        $this->tasks[$posB] = $this->tasks[$posA];
+        $this->tasks[$posA] = $tmp;
+
+        $tmp = $this->processors[$posB];
+        $this->processors[$posB] = $this->processors[$posA];
+        $this->processors[$posA] = $tmp;
+    }
+
+    public static function factory(TaskHandler $taskHandler)
+    {
+        $i = 0;
+
+        $chromosome = new self();
+
+        $size = $taskHandler->getSize();
+        $maxBoundary = $size - 1;
+
+        while ($i < $size) {
+            $gene = mt_rand(0, $maxBoundary);
+
+            if ($chromosome->hasTask($gene)) {
+                continue;
+            }
+
+            $chromosome->addTask($i, $gene, mt_rand(0, 1));
+
+            ++$i;
+        }
+
+        return $chromosome;
+    }
+}
